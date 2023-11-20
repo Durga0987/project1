454bae2e7da5fe0a651fd69cd6cd4cbc1924e518
Durga0987 <113330901+Durga0987@users.noreply.github.com>
2023-11-20 12:01:06 +0530

    Add files via upload

---
+++ nested_for.java.java
@@ -0,0 +1,12 @@
+public class nested_for {
+    public static void main(String args[]){
+        for(int i=1;i<=10;++i){
+            for(int j=10;j>=0;--j){
+                
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
